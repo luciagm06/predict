4f4d231739401721a71811481f53e3a0af8b4381
Lucia González Mandler <lgm220@alu.ua.es>
2025-12-17 14:00:53 +0100

    modified:   mongo.js

--- mongo.js
+++ mongo.js
@@ -1,20 +1,21 @@
 'use strict';
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const MONGO_URI = process.env.MONGO_URI;
 
-if (!MONGO_URI) {
-  console.error("[MONGO] Variable de entorno MONGO_URI no definida");
-  process.exit(1);
-}
-
 async function connectMongo() {
   try {
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI no definida en variables de entorno");
+    }
+
     await mongoose.connect(MONGO_URI);
-    console.log("[MONGO] Conectado a MongoDB:", MONGO_URI);
+
+    console.log("[MONGO] Conectado a MongoDB");
+    console.log("[MONGO] URI:", MONGO_URI);
   } catch (err) {
-    console.error("[MONGO] Error conectando a MongoDB:", err);
+    console.error("[MONGO] Error de conexión:", err);
     process.exit(1);
   }
 }
