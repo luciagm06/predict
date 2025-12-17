08aa90bfcff8ef0a8a28e4ff9f1a87ce4ba65b3b
Lucia González Mandler <lgm220@alu.ua.es>
2025-12-17 10:00:23 +0100

    modified:   mongo.js

--- mongo.js
+++ mongo.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const MONGO_URI = 'mongodb://localhost:27018/predict';
 
+
 async function connectMongo() {
   await mongoose.connect(MONGO_URI);
   console.log('[PREDICT] Mongo conectado');
